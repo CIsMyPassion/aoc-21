412c0724b5e850ea5292c9b1eb680526877bac05
Sebastian Nocke <sjno@gmx.de>
2021-12-11 14:37:43 +0100

    Added day 4 folder and build'n run script

---
+++ src/04/main.rs
@@ -0,0 +1,15 @@
+fn task1() {
+    println!("Result: {}", 0);
+}
+
+fn task2() {
+    println!("Result: {}", 0);
+}
+
+fn main() {
+    println!("Task 1:");
+    task1();
+
+    println!("Task 2:");
+    task2();
+}
---
+++ src/run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ "$#" != 1 ] ; then
+    echo "The build folder has to be given"
+    exit 1
+fi
+
+for d in */ ; do
+    DIR=${d::-1}
+
+    if [ "$DIR" = "$1" ] ; then
+        cd "$DIR"
+
+        rustc "main.rs"
+        ./main
+    fi
+
+done
