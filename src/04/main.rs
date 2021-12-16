48be90154199869ffa6a4ba8f018600358ae9419
Sebastian Nocke <sjno@gmx.de>
2021-12-16 13:13:55 +0100

    Work on day 4 task 1

--- src/04/main.rs
+++ src/04/main.rs
@@ -13,26 +13,85 @@ impl Board {
         Self { values: values, picked: [[false; 5]; 5] }
     }
 
+    pub fn choose_number(&mut self, number: &u8) -> bool {
+        for (i, row) in self.values.iter().enumerate() {
+            for (j, element) in row.iter().enumerate() {
+                if *element == *number {
+                    self.picked[i][j] = true;
+                    return true;
+                }
+            }
+        }
+        false
+    }
+
+    pub fn is_solved(&self) -> bool {
+
+        for i in 0..5 {
+            if self.check_row(i) {
+                return true;
+            }
+        }
+
+        for i in 0..5 {
+            if self.check_column(i) {
+                return true;
+            }
+        }
+
+        false
+    }
+
+    fn check_row(&self, index: usize) -> bool {
+        for j in 0..5 {
+            if !self.picked[index][j] {
+                return false;
+            }
+        }
+
+        true
+    }
+
+    fn check_column(&self, index: usize) -> bool {
+        for j in 0..5 {
+            if !self.picked[j][index] {
+                return false;
+            }
+        }
+
+        true
+    }
+
+    pub fn debug_print(&self) {
+        for row in self.picked.iter() {
+            let mut line: String = String::new();
+            for e in row.iter() {
+                if *e {
+                    line.push('1');
+                } else {
+                    line.push('0');
+                }
+            }
+            println!("{}", line);
+        }
+    }
 }
 
 impl FromStr for Board {
     type Err = ParseIntError;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        //println!("{}", s);
+
         let mut values: [[u8; 5]; 5] = [[0; 5]; 5];
         let rows = s.split("\n").collect::<Vec<_>>();
-        //println!("{}", rows.len());
 
         for (i, r) in rows.iter().enumerate() {
+
             let row_trim = r.trim().replace("  ", " ");
             let elements = row_trim.split(" ").collect::<Vec<_>>();
-            //println!("{}", elements.len());
-            //println!("{}", r);
 
             for (j, e) in elements.iter().enumerate() {
                 values[j][i] = e.parse::<u8>().unwrap();
-                //println!("{} {} {}", values[j][i], j, i);
             }
         }
 
@@ -78,7 +137,25 @@ fn read_data() -> (Vec<u8>, Vec<Board>) {
 
 fn task1() {
 
-    let (numbers, boards) = read_data();
+    let (numbers, mut boards) = read_data();
+    let mut solved_boards: Vec<Board> = Vec::new();
+    /*
+    for number in &numbers {
+        for board in &mut boards {
+            if board.choose_number(number) {
+                if board.is_solved() {
+
+                    solved_boards.push(*board);
+                }
+            }
+        }
+    }
+    */
+    for number in &numbers {
+        for board in &mut boards {
+            board.choose_number(number);
+        }
+    }
 
     println!("{} {}", numbers.len(), boards.len());
 
